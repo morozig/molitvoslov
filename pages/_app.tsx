4b617a59c5d918bae66bac6b0989f84575837c52
morozig@gmail.com <morozig@gmail.com>
2022-07-03 18:04:21 +0300

    fix meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,13 @@
 import { CssBaseline } from '@mui/material'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta name='viewport' content='initial-scale=1, width=device-width' />
+      </Head>
       <CssBaseline/>
       <Component {...pageProps} />
     </>
