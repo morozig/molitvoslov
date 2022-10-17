3c6a412194cdcd7ba6d53a398145eb7d1268060a
morozig@gmail.com <morozig@gmail.com>
2022-10-17 06:38:02 +0300

    translate files

--- src/prayers/son/index.tsx
+++ src/prayers/son/index.tsx
@@ -1,109 +1,109 @@
-import NaSonGryadushim from './cs/NaSonGryadushim';
-import NaSonGryadushimAlt from './ru/NaSonGryadushim';
-import Prednachinatelnaya from './cs/Prednachinatelnaya';
-import PrednachinatelnayaAlt from './ru/Prednachinatelnaya';
-import SvyatomuDuhu from './cs/SvyatomuDuhu';
-import SvyatomuDuhuAlt from './ru/SvyatomuDuhu';
-import Trisvyatoe from './cs/Trisvyatoe';
-import TrisvyatoeAlt from './ru/Trisvyatoe';
-import KoPresvyatoyTroitse from './cs/KoPresvyatoyTroitse';
-import KoPresvyatoyTroitseAlt from './ru/KoPresvyatoyTroitse';
-import Gospodnya from './cs/Gospodnya';
-import GospodnyaAlt from './ru/Gospodnya';
-import TropariTroichnie from './cs/TropariTroichnie';
-import TropariTroichnieAlt from './ru/TropariTroichnie';
-import M1 from './cs/M1';
-import M1Alt from './ru/M1';
-import M2 from './cs/M2';
-import M2Alt from './ru/M2';
-import M3 from './cs/M3';
-import M3Alt from './ru/M3';
-import M4 from './cs/M4';
-import M4Alt from './ru/M4';
-import M5 from './cs/M5';
-import M5Alt from './ru/M5';
-import M6 from './cs/M6';
-import M6Alt from './ru/M6';
-import M7 from './cs/M7';
-import M7Alt from './ru/M7';
-import M8 from './cs/M8';
-import M8Alt from './ru/M8';
-import M9 from './cs/M9';
-import M9Alt from './ru/M9';
-import M10 from './cs/M10';
-import M10Alt from './ru/M10';
-import M11 from './cs/M11';
-import M11Alt from './ru/M11';
-import KondakBogoroditse from './cs/KondakBogoroditse';
-import KondakBogoroditseAlt from './ru/KondakBogoroditse';
-import Ioannikeya from './cs/Ioannikeya';
-import IoannikeyaAlt from './ru/Ioannikeya';
-import IoannaDamaskina from './cs/IoannaDamaskina';
-import IoannaDamaskinaAlt from './ru/IoannaDamaskina';
-import Krestu from './cs/Krestu';
-import KrestuAlt from './ru/Krestu';
-import Oslabi from './cs/Oslabi';
-import OslabiAlt from './ru/Oslabi';
-import Nenavidyashih from './cs/Nenavidyashih';
-import NenavidyashihAlt from './ru/Nenavidyashih';
-import Ispovedovanie from './cs/Ispovedovanie';
-import IspovedovanieAlt from './ru/Ispovedovanie';
+import НаСонГрядущим from './cs/НаСонГрядущим';
+import НаСонГрядущимРу from './ru/НаСонГрядущим';
+import Предначинательная from './cs/Предначинательная';
+import ПредначинательнаяРу from './ru/Предначинательная';
+import СвятомуДуху from './cs/СвятомуДуху';
+import СвятомуДухуРу from './ru/СвятомуДуху';
+import Трисвятое from './cs/Трисвятое';
+import ТрисвятоеРу from './ru/Трисвятое';
+import КоПресвятойТроице from './cs/КоПресвятойТроице';
+import КоПресвятойТроицеРу from './ru/КоПресвятойТроице';
+import Господня from './cs/Господня';
+import ГосподняРу from './ru/Господня';
+import ТропариТроичные from './cs/ТропариТроичные';
+import ТропариТроичныеРу from './ru/ТропариТроичные';
+import М1 from './cs/М1';
+import М1Ру from './ru/М1';
+import М2 from './cs/М2';
+import М2Ру from './ru/М2';
+import М3 from './cs/М3';
+import М3Ру from './ru/М3';
+import М4 from './cs/М4';
+import М4Ру from './ru/М4';
+import М5 from './cs/М5';
+import М5Ру from './ru/М5';
+import М6 from './cs/М6';
+import М6Ру from './ru/М6';
+import М7 from './cs/М7';
+import М7Ру from './ru/М7';
+import М8 from './cs/М8';
+import М8Ру from './ru/М8';
+import М9 from './cs/М9';
+import М9Ру from './ru/М9';
+import М10 from './cs/М10';
+import M10Ру from './ru/М10';
+import M11 from './cs/М11';
+import М11Ру from './ru/М11';
+import КондакБогородице from './cs/КондакБогородице';
+import КондакБогородицеРу from './ru/КондакБогородице';
+import Иоанникия from './cs/Иоанникия';
+import ИоанникияРу from './ru/Иоанникия';
+import ИоннаДамаскина from './cs/ИоннаДамаскина';
+import ИоннаДамаскинаРу from './ru/ИоннаДамаскина';
+import Кресту from './cs/Кресту';
+import КрестуРу from './ru/Кресту';
+import Ослаби from './cs/Ослаби';
+import ОслабиРу from './ru/Ослаби';
+import Ненавидящих from './cs/Ненавидящих';
+import НенавидящихРу from './ru/Ненавидящих';
+import Исповедование from './cs/Исповедование';
+import ИсповедованиеРу from './ru/Исповедование';
 
-const Utro: React.FC = () => {
+const Вечерние: React.FC = () => {
   return (
     <>
-      <NaSonGryadushim/>
-      <NaSonGryadushimAlt/>
-      <Prednachinatelnaya/>
-      <PrednachinatelnayaAlt/>
-      <SvyatomuDuhu/>
-      <SvyatomuDuhuAlt/>
-      <Trisvyatoe/>
-      <TrisvyatoeAlt/>
-      <KoPresvyatoyTroitse/>
-      <KoPresvyatoyTroitseAlt/>
-      <Gospodnya/>
-      <GospodnyaAlt/>
-      <TropariTroichnie/>
-      <TropariTroichnieAlt/>
-      <M1/>
-      <M1Alt/>
-      <M2/>
-      <M2Alt/>
-      <M3/>
-      <M3Alt/>
-      <M4/>
-      <M4Alt/>
-      <M5/>
-      <M5Alt/>
-      <M6/>
-      <M6Alt/>
-      <M7/>
-      <M7Alt/>
-      <M8/>
-      <M8Alt/>
-      <M9/>
-      <M9Alt/>
-      <M10/>
-      <M10Alt/>
+      <НаСонГрядущим/>
+      <НаСонГрядущимРу/>
+      <Предначинательная/>
+      <ПредначинательнаяРу/>
+      <СвятомуДуху/>
+      <СвятомуДухуРу/>
+      <Трисвятое/>
+      <ТрисвятоеРу/>
+      <КоПресвятойТроице/>
+      <КоПресвятойТроицеРу/>
+      <Господня/>
+      <ГосподняРу/>
+      <ТропариТроичные/>
+      <ТропариТроичныеРу/>
+      <М1/>
+      <М1Ру/>
+      <М2/>
+      <М2Ру/>
+      <М3/>
+      <М3Ру/>
+      <М4/>
+      <М4Ру/>
+      <М5/>
+      <М5Ру/>
+      <М6/>
+      <М6Ру/>
+      <М7/>
+      <М7Ру/>
+      <М8/>
+      <М8Ру/>
+      <М9/>
+      <М9Ру/>
+      <М10/>
+      <M10Ру/>
       <M11/>
-      <M11Alt/>
-      <KondakBogoroditse/>
-      <KondakBogoroditseAlt/>
-      <Ioannikeya/>
-      <IoannikeyaAlt/>
-      <IoannaDamaskina/>
-      <IoannaDamaskinaAlt/>
-      <Krestu/>
-      <KrestuAlt/>
-      <Oslabi/>
-      <OslabiAlt/>
-      <Nenavidyashih/>
-      <NenavidyashihAlt/>
-      <Ispovedovanie/>
-      <IspovedovanieAlt/>
+      <М11Ру/>
+      <КондакБогородице/>
+      <КондакБогородицеРу/>
+      <Иоанникия/>
+      <ИоанникияРу/>
+      <ИоннаДамаскина/>
+      <ИоннаДамаскинаРу/>
+      <Кресту/>
+      <КрестуРу/>
+      <Ослаби/>
+      <ОслабиРу/>
+      <Ненавидящих/>
+      <НенавидящихРу/>
+      <Исповедование/>
+      <ИсповедованиеРу/>
     </>
   );
 };
 
-export default Utro;
+export default Вечерние;
