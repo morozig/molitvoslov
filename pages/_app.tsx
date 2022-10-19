ebf0af3ca1c25ef55b6a0a365a067ea6e7b70681
morozig@gmail.com <morozig@gmail.com>
2022-10-19 05:52:09 +0300

    publish info

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,28 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name='viewport' content='initial-scale=1, width=device-width' />
+        <title>
+          Молитвослов
+        </title>
+        <meta
+          name='description'
+          content='Православный молитвослов для новоначальных с
+            параллельным переводом'
+        />
+        <meta
+          property='og:image'
+          content='/molitvoslov/banner.jpg'
+        />
+        <meta
+          property='og:description'
+          content='Православный молитвослов для новоначальных с
+            параллельным переводом'
+        />
+        <meta
+          property='og:title'
+          content='Молитвослов'
+        />
+        <link rel="apple-touch-icon" sizes="180x180" href="/molitvoslov/apple-touch-icon.png"></link>
       </Head>
       <CssBaseline/>
       <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { Box, Typography } from '@mui/material'
 import type { NextPage } from 'next'
+import Head from 'next/head'
 import App from '../src/app'
 
 const Home: NextPage = () => {
